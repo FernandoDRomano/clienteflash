03ea13534932869679836bba012b00ac152b1ba8
Fernando Daniel Romano <fernando.daniel.romano.2020@gmail.com>
2025-12-03 11:08:15 -0300

    feat: eliminando comentarios del index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
     session_save_path('tmp');
-    //session_start();
-	//session_save_path('/var/cpanel/php/sessions/ea-php72');
-    //session_start();
-        /*
-    $_SESSION['color'] = 'Red';
-    */
     
 	$currentCookieParams = session_get_cookie_params();
 	$rootDomain = "http://$_SERVER[HTTP_HOST]";
@@ -37,13 +31,9 @@
 	$PasswordGet = issetornull("f");
 		
 	if($UsuarioGet != '' && $PasswordGet != ''){
-		//print_r($ruta);
 		$ruta="/clienteflash";
-		//define('URL', "http://$_SERVER[HTTP_HOST]/clienteflash/log/verificar.php");
-	}else{
-		
-		//define('URL', "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"); ////echo("(". URL .")"); // local(http://localhost:8081/correoflash/) server(http://sispo.com.ar/correoflash/)
 	}
+
 	define('URL', "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"); ////echo("(". URL .")"); // local(http://localhost:8081/correoflash/) server(http://sispo.com.ar/correoflash/)
 	if(@session_start()){//session_start() Funciona En Servidores Solo Si No Se Imprimio Nada En Pantalla antes.
 		setcookie($sess_name, session_id(), NULL, URL);
@@ -51,69 +41,34 @@
 		if($UsuarioGet != '' && $PasswordGet != ''){
 			$_SESSION['UsuarioGet'] = $UsuarioGet;
 			$_SESSION['PasswordGet'] = $PasswordGet;
-			//echo("<p>" . $UsuarioGet . "</p>");
-			//echo("<p>" . $PasswordGet . "</p>");
-		}else{
-			/*
-			if(isset($_COOKIE['us_name'])){ unset($_COOKIE['us_name']);}
-			if(isset($_COOKIE['us_password'])){ unset($_COOKIE['us_password']);}
-			if(isset($_COOKIE['idusuario'])){ unset($_COOKIE['idusuario']);}
-			if(isset($_COOKIE['us_nombre'])){ unset($_COOKIE['us_nombre']);}
-			if(isset($_COOKIE['us_apellido'])){ unset($_COOKIE['us_apellido']);}
-			if(isset($_COOKIE['idperfil'])){ unset($_COOKIE['idperfil']);}
-			if(isset($_COOKIE['UsuarioNombreDeMenu'])){ unset($_COOKIE['UsuarioNombreDeMenu']);}
-			if(isset($_COOKIE['UsuarioMainMenu'])){ unset($_COOKIE['UsuarioMainMenu']);}
-			if(isset($_COOKIE['UsuarioURL'])){ unset($_COOKIE['UsuarioURL']);}
-			if(isset($_SESSION["UsuarioGet"])){unset($_SESSION["UsuarioGet"]);}
-			if(isset($_SESSION["PasswordGet"])){unset($_SESSION["PasswordGet"]);}
-			if(isset($_SESSION['us_name'])){ unset($_SESSION['us_name']);}
-			if(isset($_SESSION['us_password'])){ unset($_SESSION['us_password']);}
-			if(isset($_SESSION['idusuario'])){ unset($_SESSION['idusuario']);}
-			if(isset($_SESSION['us_nombre'])){ unset($_SESSION['us_nombre']);}
-			if(isset($_SESSION['us_apellido'])){ unset($_SESSION['us_apellido']);}
-			if(isset($_SESSION['idperfil'])){ unset($_SESSION['idperfil']);}
-			if(isset($_SESSION['UsuarioNombreDeMenu'])){ unset($_SESSION['UsuarioNombreDeMenu']);}
-			if(isset($_SESSION['UsuarioMainMenu'])){ unset($_SESSION['UsuarioMainMenu']);}
-			if(isset($_SESSION['UsuarioURL'])){ unset($_SESSION['UsuarioURL']);}
-			*/
 		}
 	}
 	$PCname=gethostname();
-	//echo($_SERVER['REQUEST_URI']);
-	//print_r($_POST);
-	//print_r($_GET);
-	//print_r($_REQUEST);
 ?>
 <script>
 	//var SinSobdominio = true;
 	var PCname = <?php echo(json_encode($PCname));?>;
 	var SinSobdominio;
+
 	if(PCname == "Ruben" || PCname == "RubenGF" || PCname == "ded1521.inmotionhosting.com"){
 		SinSobdominio = false;
 	}else{
-		//alert(PCname);
 		SinSobdominio = true;
 	}
-	//alert(PCname);
+
 	var SUBDOMINIO = window.location.pathname.substring( 0 , window.location.pathname.indexOf("/",1) + 1 );
 	var SqlServerURLJS = "http://sispo.com.ar/" + "clienteflash/";
 	
 	if(PCname == "Ruben" || PCname == "RubenGF"){
 		var Boveda = window.location.origin + "/boveda";
 	}else{
-		//alert(PCname);
 		var Boveda = "https://apis.sppflash.com.ar";
 	}
+
 	if(!SinSobdominio){
 		var URLJS = window.location.origin + SUBDOMINIO;
-		
-		//var Boveda = "http://apis.sppflash.com.ar";
 	}else{
 		var URLJS = window.location.origin + "/";
-		//alert(window.location.origin);
-		//alert(window.location.origin.indexOf('.',0));
-		//alert(window.location.origin.substring(window.location.origin.indexOf('.',0),0));
-		
 	}
 	
 	function IncludeJs(jsFilePath, id){
@@ -186,14 +141,9 @@
 	}if(isset($_SESSION['UsuarioMainMenu'])){
 		$UsuarioNombreDeMenu = $_SESSION['UsuarioMainMenu'];
 	}
-	
-	//print_r($_COOKIE["UsuarioURL"]);
-	//print_r($_SESSION);
 		
 	Config\Autoload::run();
 	$Request = new Config\Request();
-	
-	
 	
 	if(isset($_SESSION['us_name'])){ 
 		$NombreDeUsuario = $_SESSION['us_name'];
@@ -202,45 +152,13 @@
 		$Perfil = $_SESSION['idperfil'];
 	}
 	
-	//if($ruta != "/" . SUBDOMINIO . "/" && $ruta != "/" . SUBDOMINIO . "/" . "log/verificar" && isset($_SESSION['us_name']) && isset($_SESSION['us_password'])){
-		if($Request->getMenu() != ''){
-			require_once $Request->getMenu();//"Views/menu.php";
-		}
-
-	//}
-	/*
-	//echo("Usuario:" . $UsuarioGet . " Menu:" . $Request->getMenu() !="");
-	if($UsuarioGet == '' && $PasswordGet == '' && !isset($_GET['url'])){
-		if(isset($_COOKIE['us_name'])){ unset($_COOKIE['us_name']);}
-		if(isset($_COOKIE['us_password'])){ unset($_COOKIE['us_password']);}
-		if(isset($_COOKIE['idusuario'])){ unset($_COOKIE['idusuario']);}
-		if(isset($_COOKIE['us_nombre'])){ unset($_COOKIE['us_nombre']);}
-		if(isset($_COOKIE['us_apellido'])){ unset($_COOKIE['us_apellido']);}
-		if(isset($_COOKIE['idperfil'])){ unset($_COOKIE['idperfil']);}
-		if(isset($_COOKIE['UsuarioNombreDeMenu'])){ unset($_COOKIE['UsuarioNombreDeMenu']);}
-		if(isset($_COOKIE['UsuarioMainMenu'])){ unset($_COOKIE['UsuarioMainMenu']);}
-		if(isset($_COOKIE['UsuarioURL'])){ unset($_COOKIE['UsuarioURL']);}
-		if(isset($_SESSION["UsuarioGet"])){unset($_SESSION["UsuarioGet"]);}
-		if(isset($_SESSION["PasswordGet"])){unset($_SESSION["PasswordGet"]);}
-		if(isset($_SESSION['us_name'])){ unset($_SESSION['us_name']);}
-		if(isset($_SESSION['us_password'])){ unset($_SESSION['us_password']);}
-		if(isset($_SESSION['idusuario'])){ unset($_SESSION['idusuario']);}
-		if(isset($_SESSION['us_nombre'])){ unset($_SESSION['us_nombre']);}
-		if(isset($_SESSION['us_apellido'])){ unset($_SESSION['us_apellido']);}
-		if(isset($_SESSION['idperfil'])){ unset($_SESSION['idperfil']);}
-		if(isset($_SESSION['UsuarioNombreDeMenu'])){ unset($_SESSION['UsuarioNombreDeMenu']);}
-		if(isset($_SESSION['UsuarioMainMenu'])){ unset($_SESSION['UsuarioMainMenu']);}
-		if(isset($_SESSION['UsuarioURL'])){ unset($_SESSION['UsuarioURL']);}
+	if($Request->getMenu() != ''){
+		require_once $Request->getMenu();//"Views/menu.php";
 	}
-	*/
+
 	$URLJS =  Ajax . $Request->getJsDeMenu() . '.js';
-	//echo("<P>" . $URLJS . "</P>");
 	Config\Enrutador::run($Request);
-	/*
-	print_r($UsuarioGet);
-	print_r($PasswordGet);
-	exit;
-	*/
+
 	$pos = strpos($ruta, "XMLHttpRequest");
 	if(is_readable($URLJS)){
 		$Js = file_get_contents( $URLJS );
@@ -248,14 +166,6 @@
 	}else{
 		print_r("Fichero No Encontrado:" . $URLJS);
 	}
-	
-	/*
-    print_r('_SESSION =');
-    print_r($_SESSION);
-    print_r('_COOKIE=');
-    print_r($_COOKIE);
-    */
-	//include 'h_footer.php';
 ?>
 
 
