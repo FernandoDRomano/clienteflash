bb196e107a8db7ae3d0205ce3b21b46d52ed753a
Fernando Daniel Romano <fernando.daniel.romano.2020@gmail.com>
2026-01-05 12:17:12 -0300

    feat: agregar método filtrar en la clase Pieza para mejorar la búsqueda de piezas con filtros específicos

--- Models/Pieza.php
+++ Models/Pieza.php
@@ -63,8 +63,121 @@ class Pieza {
             
             return $result;
         } catch (Exception $e) {
-            $this->log->exception("Error al crear pieza: ", $e->getMessage());
+            $this->log->exception("Error al crear pieza", $e);
             throw new Exception("Ocurrió un error al crear la pieza.");
+        }
+    }
+
+    public function filtrar($filtros, $con = null){
+        try {
+            if (!$con) {
+                $con = new ConexionSispo();
+            }
+
+            $whereClauses = [];
+
+            $this->log->info("Pieza",  "Iniciando filtrado de piezas con los siguientes filtros: ", [
+                'filtros' => $filtros
+            ]);
+
+            if(empty($filtros['clienteId'])) {
+                $this->log->error("Pieza", "El filtro 'clienteId' es obligatorio.");
+                throw new Exception("El filtro 'clienteId' es obligatorio.");
+            }
+
+            $whereClauses[] = "fci.cliente_id = '" . $con->escapeString($filtros['clienteId']) . "'";
+
+            if(!empty($filtros['BarcodeExterno'])) {
+                $NumeroDePieza = $con->escapeString($filtros['BarcodeExterno']);
+                $whereClauses[] = "fp.barcode_externo = '$NumeroDePieza'";
+            } 
+
+            if(!empty($filtros['Documento'])) {
+                $DNIBusqueda = $con->escapeString($filtros['Documento']);
+                $whereClauses[] = "cd.ApoderadoDocumento = '$DNIBusqueda'";
+            }
+
+            if(!empty($filtros['ApellidoYNombre'])) {
+                $Destinatario = $con->escapeString($filtros['ApellidoYNombre']);
+                $whereClauses[] = "fp.destinatario LIKE '%$Destinatario%'";
+            }
+
+            if(!empty($filtros['FechaI'])) {
+                $Desde = $con->escapeString($filtros['FechaI']) . " 00:00:00";
+                $whereClauses[] = "fci.create >= '$Desde'";
+            } 
+
+            if(!empty($filtros['FechaF'])) {
+                $Hasta = $con->escapeString($filtros['FechaF']) . " 23:59:59";
+                $whereClauses[] = "fci.create <= '$Hasta'";
+            }
+
+            if(!empty($filtros['piezaId'])) {
+                $piezaId = $con->escapeString($filtros['piezaId']);
+                $whereClauses[] = "fp.id = $piezaId";
+            }
+
+            $whereSql = '';
+            if (count($whereClauses) > 0) {
+                $whereSql = 'WHERE ' . implode(' AND ', $whereClauses);
+            }
+            
+            $sql = "SELECT 
+            	fp.barcode_externo AS 'Barcode'
+                ,fpt.create AS 'FechaDeEstado'
+                ,fpt.estado_id AS 'idEstado'
+                ,fp.id AS 'idPieza'
+                ,fp.id
+                ,fev.nombre AS 'NombreDeEstado'
+                ,fp.estado_id AS 'UltimoEstado'
+                ,fp.update AS 'FechaDeUltimoEstado'
+                ,fuev.nombre AS 'NombreDeUltimoEstado'
+                ,fs.nombre AS 'Sucursal'
+                ,RTRIM(fp.destinatario) AS 'Destinatario'
+                ,RTRIM(fp.domicilio) AS 'Direccion de entrega'
+                ,fp.codigo_postal AS 'Cp'
+                ,fp.localidad AS 'Localidad'
+                ,fp.documento AS 'documento'
+                ,fp.recibio AS 'recibio'
+            	,CASE
+            		WHEN fp.vinculo LIKE 'APP-%' THEN SUBSTRING(fp.vinculo, 5)
+            		ELSE fp.vinculo
+            	END AS 'Vinculo'
+                ,datos.fichero AS 'FotoDeAcuse'
+                ,fev.flash_piezas_estados_declarados AS 'flash_piezas_estados_declarados'
+            FROM
+            	flash_piezas AS fp
+                INNER JOIN flash_piezas_estados_variables fuev on fuev.id = fp.estado_id
+                INNER JOIN flash_comprobantes_ingresos AS fci ON fci.id = fp.comprobante_ingreso_id
+                INNER JOIN flash_comprobantes_ingresos_servicios AS fcis ON fcis.id = fp.servicio_id
+                INNER JOIN flash_piezas_tracking AS fpt ON fpt.pieza_id  = fp.id
+                INNER JOIN flash_piezas_estados_variables fev on fev.id = fpt.estado_id
+                LEFT JOIN flash_datos_estados AS datos ON datos.flash_piezas_tracking_id  = fpt.id
+                LEFT JOIN flash_piezas_cd AS cd ON cd.IdFlashPieza = fp.id
+                LEFT JOIN flash_sucursales AS fs ON fs.id = fp.sucursal_id
+            $whereSql
+            ORDER BY
+                fp.barcode_externo ,
+                fp.id,
+                fpt.create ASC;";
+
+            $this->log->info("Pieza",  "Filtrando piezas con los siguientes filtros: ", [
+                'filtros' => $filtros,
+                'sql' => preg_replace('/\s+/', ' ', trim($sql))
+            ]);
+
+            $result = $con->consultaRetorno($sql);
+            $piezas = [];
+
+            while ($row = $result->fetch_row()) {
+                $piezas[] = $row;
+            }
+
+            return $piezas;
+
+        } catch (Exception $e) {
+            $this->log->exception("Error al filtrar piezas", $e);
+            throw new Exception("Ocurrió un error al filtrar las piezas.");
         }
     }
 }
